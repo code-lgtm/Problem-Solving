533b5af6cd640d8d68c6bcad0b4b4f90c38a97c5
Prashant Mishra <inbox.prashant@gmail.com>
2018-11-17 22:36:46 +0530

    MSPaint fill color

---
+++ java/com/test/FillPaint.java
@@ -0,0 +1,62 @@
+package com.test;
+
+public class FillPaint {
+//fill like msPaint fill functionlity 
+    /*
+    Output:
+1 1 1 1 1 1 2 2
+1 1 1 1 1 1 0 0
+1 0 0 1 1 0 1 1
+1 2 3 3 3 0 1 0
+1 1 1 3 3 0 1 0
+1 1 1 3 3 3 3 0
+1 1 1 1 1 3 1 1
+1 1 1 1 1 3 3 1
+     */
+
+    public static void main(String[] args){
+        int[][] ip = {{1, 1, 1, 1, 1, 1, 2,2},
+                {1, 1, 1, 1, 1, 1, 0, 0},
+                {1, 0, 0, 1, 1, 0, 1, 1},
+                {1, 2, 2, 2, 2, 0, 1, 0},
+                {1, 1, 1, 2, 2, 0, 1, 0},
+                {1, 1, 1, 2, 2, 2, 2, 0},
+                {1, 1, 1, 1, 1, 2, 1, 1},
+                {1, 1, 1, 1, 1, 2, 2, 1},
+        };
+
+        int[][] sol = fillPaint(ip, 2, 3, 4, 3);//replace color 2 with 3, starting pixel @ 4,3
+        print(sol);
+    }
+
+    static int[][] fillPaint(int[][] ip,int currentColor, int newColor, int x, int y){
+        //
+
+        if(x >= 0 && y >= 0 && x < ip.length && y < ip[0].length && ip[x][y] == currentColor){
+            ip[x][y] = newColor;
+        }else{
+            return ip;
+        }
+
+
+        int[][] newPaint = fillPaint(ip, currentColor, newColor, x+1, y);
+        newPaint = fillPaint(newPaint, currentColor, newColor, x, y+1);
+        newPaint = fillPaint(newPaint, currentColor, newColor, x-1, y);
+        newPaint = fillPaint(newPaint, currentColor, newColor, x-1, y-1);
+
+        return newPaint ;
+
+
+    }
+
+    static boolean print(int[][] sol){
+        for(int i = 0; i < sol.length; i++){
+            for(int j =0; j <sol[0].length; j++){
+                System.out.print(sol[i][j]+" ");
+            }
+            System.out.println();
+        }
+        return true;
+    }
+
+}
