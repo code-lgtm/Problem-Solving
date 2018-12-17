8c00a05907fa95f8c1989731e8e1a84697e49ebd
Prashant Mishra <prash-mi@yahoo.com>
2018-12-17 23:50:32 +0530

    ASCII based encryption decryption

---
+++ java/com/test/EncryptDecrypt.java
@@ -0,0 +1,83 @@
+package com.test;
+/*
+
+Question:
+
+Decrypt Message
+An infamous gang of cyber criminals named “The Gray Cyber Mob”, which is behind many hacking attacks and drug trafficking, has recently become a target for the FBI. After intercepting some of their messages, which looked like complete nonsense, the agency learned that they indeed encrypt their messages, and studied their method of encryption.
+
+Their messages consist of lowercase latin letters only, and every word is encrypted separately as follows:
+
+Convert every letter to its ASCII value. Add 1 to the first letter, and then for every letter from the second one to the last one, add the value of the previous letter. Subtract 26 from every letter until it is in the range of lowercase letters a-z in ASCII. Convert the values back to letters.
+
+For instance, to encrypt the word “crime”
+
+Decrypted message:	c	r	i	m	e
+            Step 1:	99	114	105	109	101
+            Step 2:	100	214	319	428	529
+            Step 3:	100	110	111	116	113
+Encrypted message:	d	n	o	t	q
+
+
+ */
+class EncryptDecrypt {
+
+    static String decrypt(String word) {
+
+        char[] ip = word.toCharArray();
+        String op = "";
+
+        int seed = 1;
+        for(int i = 0; i < ip.length; i++){
+
+           int tempSeed = ip[i] - seed;
+
+
+            while(! (tempSeed >= 97 && tempSeed <= 122)){
+                tempSeed += 26;
+            }
+
+            seed += tempSeed;
+            System.out.print((char) tempSeed);
+
+        }
+
+        return op;
+
+    }
+
+    static String encrypt(String word) {
+
+        char[] ip = word.toCharArray();
+        String op = "";
+
+        int seed = 1;
+        for(int i = 0; i < ip.length; i++){
+
+            seed = ip[i] + seed;
+            int tempSeed = seed;
+
+            while(! (tempSeed >= 97 && tempSeed <= 122)){
+                tempSeed -= 26;
+            }
+
+            System.out.print((char) tempSeed);
+
+        }
+
+        return op;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(encrypt("crime"));
+
+        System.out.println(decrypt("dnotq"));
+
+
+    }
+
+
+}
