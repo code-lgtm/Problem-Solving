683683fb4c3750b5c2969d0d78fad3ae2c270453
Prashant Mishra <inbox.prashant@gmail.com>
2019-01-05 20:07:31 +0530

    Clone a Un directed Directed Graph which may contain cycle

---
+++ java/epi/graph/CloneUnDirectedGraph.java
@@ -0,0 +1,109 @@
+package epi.graph;
+
+import java.util.*;
+
+public class CloneUnDirectedGraph {
+
+    private static class GNode{
+        char value;
+        List<GNode> adj = new LinkedList<>();
+        public GNode(char value){
+            this.value = value;
+        }
+    }
+
+    static Set<GNode> visited = new HashSet<GNode>();
+
+    public static void main(String[] args) {
+            /*
+Graph:
+    a     d
+    | \   |
+    |   \ |
+    b -- c
+    */
+
+        GNode a = new GNode('a');
+        GNode b = new GNode('b');
+        GNode c = new GNode('c');
+        GNode d = new GNode('d');
+
+        a.adj.add(b);
+        b.adj.add(a);
+        a.adj.add(c);
+        c.adj.add(a);
+        c.adj.add(b);
+        b.adj.add(c);
+        c.adj.add(d);
+        d.adj.add(c);
+
+        printBFS(a);
+        GNode clonedGraph = cloneGraph(a);
+        printBFS(clonedGraph);
+/*
+Output:
+
+a @ 1627674070  b @ 1360875712  c @ 1625635731  d @ 1580066828
+a @ 491044090  b @ 644117698  c @ 1872034366  d @ 1581781576
+ */
+
+
+    }
+
+    static GNode cloneGraph(GNode src){
+
+        Queue<GNode> bfs = new LinkedList<>();
+        Map<GNode, GNode> gToClone = new HashMap<>();//maintains Gnode : Gnode's clone's mapping
+        GNode clnSrc = new GNode (src.value);
+        bfs.add(src);
+        gToClone.put(src, clnSrc);
+
+        while (! bfs.isEmpty()){
+            GNode cur = bfs.remove();
+            if(visited.contains(cur)){
+                continue;
+            }
+            visited.add(cur);
+
+            for(GNode adj: cur.adj){
+                GNode clonedCur = gToClone.get(cur);
+
+                GNode cloneAdj = null;
+                if(gToClone.containsKey(adj)){//if Adj has already been cloned then use the same object
+                    cloneAdj = gToClone.get(adj);
+                }else{// create a new adj object
+                    cloneAdj = new GNode(adj.value);
+                    gToClone.put(adj, cloneAdj);
+                }
+
+
+                clonedCur.adj.add(cloneAdj);//added a cloned adj to the cloned graph
+
+                bfs.add(adj);
+            }
+        }
+
+        return clnSrc;
+    }
+
+    static void printBFS(GNode node){
+
+        Set<GNode> visited = new HashSet<GNode>();
+        Queue<GNode> bfs = new LinkedList<>();
+        bfs.add(node);
+        visited.add(node);
+
+        while (!bfs.isEmpty()){
+            GNode cur = bfs.remove();
+            System.out.print(cur.value +" @ "+cur.hashCode()+"  ");
+            for(GNode adj: cur.adj){
+                if(! visited.contains(adj)){
+                    bfs.add(adj);
+                    visited.add(adj);
+                }
+            }
+
+        }
+        System.out.println();
+    }
+}
