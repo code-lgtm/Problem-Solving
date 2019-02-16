d87c34c197ade6a9aee180fadcd68c2d0bd08699
Prashant Mishra <inbox.prashant@gmail.com>
2019-02-16 17:20:10 +0530

    Check if a tree is a BST in O(n) time and O(log n) space

---
+++ java/epi/bst/IsBST.java
@@ -0,0 +1,55 @@
+package epi.bst;
+
+//Check if a tree is a BST in O(n) time and O(log n) space
+public class IsBST {
+
+    static class BNode{
+        BNode left, right;
+        int value;
+        BNode(int value){
+            this.value= value;
+        }
+    }
+    public static void main(String[] args) {
+        int[] ar = {-1, 2, 3, 7, 8, 9, 50, 100};
+
+        BNode root = getTree(ar, 0, ar.length - 1);
+        System.out.println(isBST(root));
+
+        root.right.right.value = -1;
+        System.out.println(isBST(root));
+
+        //Output: true
+        //        false
+    }
+
+    static boolean isBST(BNode cur){
+        if(cur == null){
+            return true;
+        }
+        if(cur.left!= null && cur.left.value > cur.value){
+            return false;
+        }
+        if (cur.right!= null && cur.right.value < cur.value){
+            return false;
+        }
+        boolean isLeftBST = isBST(cur.left);
+        boolean isRightBST = isBST(cur.right);
+
+        return isLeftBST && isRightBST;
+    }
+
+    static BNode getTree(int[] ip, int start, int end){
+        if(start > end){
+            return null;
+        }
+
+        int mid = (start + end)/2;
+        BNode node = new BNode(ip[mid]);
+        node.left = getTree(ip, start, mid-1);
+        node.right = getTree(ip, mid+1, end);
+
+        return node;
+
+    }
+}
