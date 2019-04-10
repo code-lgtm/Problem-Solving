f2275563c483cba97cafb3b2d97017760f79aefa
Kumar Gaurav <kumargarg@google.com>
2019-04-10 09:38:19 +0800

    Longest Common Subsequence

--- graduate_algorithms/longest_common_subsequence.py
+++ graduate_algorithms/longest_common_subsequence.py
@@ -5,21 +5,38 @@ TASK: Longest Common Subsequence
 """
 
 def longestCommonSubsequence(a, b):
-  L = [[(0, 0, 0) for _ in range(len(b)+1)] for _ in range(len(a)+1)]
+  len_a = len(a)
+  len_b = len(b)
 
-  for i in range(1, len(a)+1):
-    for j in range(1, len(b)+1):
+  if not len_a or not len_b:
+    return 0
+
+  L = [[0 for _ in range(len_b+1)] for _ in range(len_a+1)]
+
+  for i in range(1, len_a+1):
+    for j in range(1, len_b+1):
       if a[i-1] == b[j-1]:
-        L[i][j] = (1 + L[i-1][j-1], i-1, j-1)
+        L[i][j] = 1 + L[i-1][j-1]
       else:
-        if L[i-1][j] > L[i][j-1]:
-          L[i][j] = (L[i-1][j], i-1, j)
-        else:
-          L[i][j] = (L[i][j-1], i, j-1)
+          L[i][j] = max(L[i-1][j], L[i][j-1])
 
-  return L[len(a)][len(b)]
+  max_len = L[len_a][len_b]
+  i = len_a
+  j = len_b
+  seq = []
 
+  while max_len > 0:
+    if L[i][j] == L[i-1][j-1]+1 and  a[i-1] == b[j-1]:
+      seq.append(a[i-1])
+      max_len -= 1
+      i = i-1
+      j = j-1
+    elif i > 0 and L[i-1][j] == L[i][j]:
+      i = i-1
+    else:
+      j = j-1
 
+  return list(reversed(seq))
 
 # assert longestCommonSubsequence([1, 2, 3, 4, 1], [3, 4, 1, 2, 1, 3]) == [1, 2, 3]
 print longestCommonSubsequence([1, 2, 3, 4, 1], [3, 4, 1, 2, 1, 3])
